2b6623d3009852b816ccac3d5368d90f782868ef
Aditya Paul <aditya.paul18@gmail.com>
2021-09-08 15:44:41 +0530

    validators added for email login

--- frontend/src/Conponents/Login/Login.js
+++ frontend/src/Conponents/Login/Login.js
@@ -12,8 +12,8 @@ function Login() {
     const history = useHistory()
 
     const ff = () => {
-        if (user)
-            history.push('/home')
+        // if (user)
+        //     history.push('/home')
     }
 
     useEffect(() => {
@@ -25,39 +25,49 @@ function Login() {
         auth.signInWithPopup(provider)
             .then((res) => {
                 const token = res.credential.accessToken;
-                // The signed-in user info.
                 const name = res.user.displayName;
                 const email = res.user.email;
                 axios.post('/google/login', { name, email, token })
                     .then((res) => {
                         console.log(res.data)
                         localStorage.setItem("CConUser", res.data.token)
-                        alert(res.data.message)
                         history.push('/home')
+                        // alert(res.data.message)
                     })
-                    .catch((err) => console.log(err))
+                    .catch((err) => alert("something went wrong"))
             })
-            .catch((err) => console.log(err))
+            .catch((err) => alert("something went wrong"))
     }
 
     const signin2 = (e) => {
         e.preventDefault();
-        const name = username
+        if( mail==="" || Password==="" || !/\S/.test(mail)|| !/\S/.test(Password)){
+            alert("Please fill in the feilds correctly")
+            return;
+        }
         const email = mail
         const password = Password
         axios.post('/login',{ email, password })
         .then((res) => {
             console.log(res.data)
             localStorage.setItem("CConUser", res.data.token)
-            alert(res.data.message)
+            // alert(res.data.message)
             history.push('/home')
         })
-        .catch((err) => console.log(err))  
+        .catch((err) => alert("Credentials do not match"))  
 
     }
 
     const signup = async (e) => {
         e.preventDefault();
+        if(Password !== passwordc ){
+            alert("Both passwords should match , Please fill in the feilds correctly")
+            return;
+        }
+        if(!username || mail==="" || Password==="" || passwordc==="" || !/\S/.test(username)|| !/\S/.test(mail)|| !/\S/.test(Password)|| !/\S/.test(passwordc)){
+            alert("Please fill in all the feilds correctly")
+            return;
+        }
         const name = username
         const email = mail
         const password = Password
