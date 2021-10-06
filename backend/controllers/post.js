24ffe6dc93237bba77e6dc8daa96a4fc19c1bc01
root <root@kali.qcpl.in>
2021-10-06 13:24:54 +0530

    Add comments and answers

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -10,7 +10,7 @@ exports.createPost = async(req, res) => {
   try{
 
     let { title, description, tags } = req.body;
-    if(!title) return res.status(400).json({error:'Post title cannot be empty'});
+    if(!title||title=="") return res.status(400).json({error:'Post title cannot be empty'});
     if(req.files){
       var imageArray= req.files.map((f)=>{
          var fileName =shortid.generate() + path.extname(f.originalname);
@@ -35,3 +35,58 @@ exports.createPost = async(req, res) => {
   }
 }
 
+exports.addAnswer = async(req, res) => {
+  try{
+    let { postId } = req.params;
+    let { description } = req.body;
+    if(!postId) return res.status(400).json({error:'Post Id is required'});
+    if(!description||description=="") return res.status(400).json({error:'Answer description cannot be empty'});
+    let existingPost = await Post.findById(postId);
+    if(!existingPost) return res.status(400).json({error:'Post not found!'});
+    if(req.files){
+      var imageArray= req.files.map((f)=>{
+         var fileName =shortid.generate() + path.extname(f.originalname);
+          bucket.file(fileName).createWriteStream().end(f.buffer)
+            const url = `https://firebasestorage.googleapis.com/v0/b/${firebaseConfig.storageBucket}/o/${fileName}?alt=media`;
+            return url;
+        });
+        req.body.images=imageArray;
+    }
+    req.body.user = req.user._id;
+    await existingPost.answer.push(req.body);
+    await existingPost.save();
+ 
+     res.status(200).json({
+       message: 'Answer added to post successfully',
+       existingPost
+     })    
+  }catch(error){
+    console.log(error);
+    res.status(500).json({error:"Something went wrong!"})
+  }
+}
+
+exports.addComment = async(req, res) => {
+  try{
+    let { postId, answerId, comment } = req.body;
+    if(!postId) return res.status(400).json({error:'Post Id is required'});
+    if(!answerId) return res.status(400).json({error:'Answer Id is required'});
+    if(!comment||comment=="") return res.status(400).json({error:'Comment cannot be empty'});
+    let existingPost = await Post.findById(postId);
+    if(!existingPost) return res.status(400).json({error:'Post not found!'});
+    await existingPost.answer.map((a,i)=>{
+      if(a._id==answerId){
+        a.comments.push({comment,user:req.user._id})
+      }
+    })
+    await existingPost.save();
+ 
+     res.status(200).json({
+       message: 'Comment added to answer successfully',
+       existingPost
+     })    
+  }catch(error){
+    console.log(error);
+    res.status(500).json({error:"Something went wrong!"})
+  }
+}
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authToken = require('../middleware/authToken');
 const {
-  createPost
+  createPost,
+  addAnswer,
+  addComment
 }= require('../controllers/post');
 const multer = require('multer');
 const upload=multer({storage: multer.memoryStorage()})
 
 router.post('/question',authToken,upload.array('images'),createPost);
+router.put('/answer/:postId',authToken,upload.array('images'),addAnswer);
+router.put('/comment',authToken, addComment);
 
 module.exports = router;
