229b46d6b197cb9ba6ccb0d42f9cfcd7e5349713
Aditya Paul <aditya.paul18@gmail.com>
2021-11-16 09:58:49 +0530

    save post fix

--- frontend/src/Conponents/Saved/HomePost.js
+++ frontend/src/Conponents/Saved/HomePost.js
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core'
+import { Button, MenuItem, TextField } from '@material-ui/core'
 import React from 'react'
 import AddIcon from '@material-ui/icons/Add';
 import AddCommentIcon from '@material-ui/icons/AddComment';
@@ -10,7 +10,16 @@ function HomePost({setopen}) {
     const history = useHistory()
     return (
         <div className="ProfilePost" >
-            <div className="PostTop"><img alt="" className="PostLogo" src="https://qph.fs.quoracdn.net/main-thumb-282129127-200-wdsefxcvsewcnoifsgtqymhoydgblwha.jpeg" /><div><span className="PostHeadName">Aditya Paul</span><span className="PostHeadCollege">3rd year student at Indian Institute of Information Technology, Surat</span></div></div>
+            {/* <div className="PostTop"><img alt="" className="PostLogo" src="https://qph.fs.quoracdn.net/main-thumb-282129127-200-wdsefxcvsewcnoifsgtqymhoydgblwha.jpeg" /><div><span className="PostHeadName">Aditya Paul</span><span className="PostHeadCollege">3rd year student at Indian Institute of Information Technology, Surat</span></div></div> */}
+            <div className="PostTop">
+                <div className="PostTop">
+                    <img alt="" className="PostLogo" src="https://qph.fs.quoracdn.net/main-thumb-282129127-200-wdsefxcvsewcnoifsgtqymhoydgblwha.jpeg" /><div><span className="PostHeadName"></span><span className="PostHeadCollege"></span></div>
+                </div>
+                <TextField value=":" className="optionMenu" select>
+                    <MenuItem value="Edit">Edit</MenuItem>
+                    <MenuItem value="Delete">Delete</MenuItem>
+                </TextField>
+            </div>
             <div className="PostAnswer">
                 <h2>Question main heading here </h2>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
