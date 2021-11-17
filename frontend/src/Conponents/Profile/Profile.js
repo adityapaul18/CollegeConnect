d8529735df2d0d1ca47eb4723eef3bc897cf01c7
Aditya Paul <aditya.paul18@gmail.com>
2021-11-17 16:46:34 +0530

    remove savded post

--- frontend/src/Conponents/Profile/Profile.js
+++ frontend/src/Conponents/Profile/Profile.js
@@ -113,20 +113,20 @@ function Profile(props) {
                         <div className="ProfileSubInfo">{user?.academicYear}</div>
                     </div>
 
-
+{/* 
                     <Tabs>
                         <TabList>
                             <Tab>Posts</Tab>
                             {userID == id && <Tab>Saved</Tab>}
                         </TabList>
 
-                        <TabPanel>
+                        <TabPanel> */}
                             <div className="SavedPosts">
                                 <h2>Your Posts</h2>
                                 {posts ? posts.length == 0 ? <h3>No posts yet!</h3> : posts.map((post) => <ProfilePost fetchPosts={fetchUserPosts}editModal={editModal} setEditModal={setEditModal} editData={editData} setEditData={setEditData} setopen2={setopen2} post={post} setModal={setModal} savedPosts={savedPosts} setSavedPosts={setSavedPosts} fetchSavedPosts={fetchSavedPosts}/>) : <h6>Loading...</h6>}
 
                             </div>
-                        </TabPanel>
+                        {/* </TabPanel>
                         {userID == id && <TabPanel>
                             <div className="SavedPosts">
                                 <h2> Your Saved Posts</h2>
@@ -134,10 +134,9 @@ function Profile(props) {
                                   return(<><ProfilePost fetchPosts={fetchSavedPosts} setopen2={setopen2} post={p} setModal={setModal} savedPosts={savedPosts} setSavedPosts={setSavedPosts} fetchSavedPosts={fetchSavedPosts}/></>)
                                 }):<h3>Loading...</h3>}
 
-                                {/* <ProfilePost /> */}
                             </div>
                         </TabPanel>}
-                    </Tabs>
+                    </Tabs> */}
 
                 </div>
             </div>
